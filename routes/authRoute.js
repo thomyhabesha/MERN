0524acfe04869de3b721906f667f904fb7482959
thomas <tafethomas07@gmail.com>
2025-04-29 10:10:07 +0300

    security concerns (2)

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,10 +4,10 @@ import {register, login, logout} from '../controllers/authController.js'
 import { validateRegisterInput, validateLoginInput } from '../middleware/validationMiddleware.js';
 import rateLimiter from 'express-rate-limit';
 
-//makes user retry after 15 min if incorrect for max 8 try
+//makes user retry after 15 min if incorrect for max 5 try
 const apiLimiter = rateLimiter({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 8,
+  max: 5,
   message: { msg: 'IP rate limit exceeded, retry in 15 minutes.' },
 });
 
